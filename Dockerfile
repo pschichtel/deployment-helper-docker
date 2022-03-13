0ca4203fba1045d481d32d62da1b393886460ca4
Phillip Schichtel <phillip@schich.tel>
2022-03-13 22:34:27 +0100

    add process-updates script

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ RUN apk add --update --no-cache bash jq python3 docker-cli git curl
 
 COPY trigger.sh /usr/local/bin/trigger
 COPY discover-descriptors.sh /usr/local/bin/discover-descriptors
+COPY process-updates.sh /usr/local/bin/process-updates
 
---
+++ process-updates.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+update_script="${1?no update script}"
+
+source_project="${ARTIFACT_SOURCE:-}"
+source_ref="${ARTIFACT_SOURCE_REF:-}"
+
+if [ -z "$source_project" ]
+then
+    echo "No source project given!"
+    exit 1
+fi
+
+if [ -z "$source_ref" ]
+then
+    echo "No source ref given!"
+    exit 1
+fi
+
+aggregated_descriptors="${ARTIFACT_DESCRIPTORS:-}"
+
+updates_file="$(mktemp)"
+if [ -n "$aggregated_descriptors" ]
+then
+    echo "Using pre-collected descriptors!"
+    echo "$aggregated_descriptors" > "$updates_file"
+else
+    echo "Discovering descriptors!"
+    discover-descriptors > "$updates_file"
+fi
+
+artifact_count="$(jq 'length' < "$updates_file")"
+if [ "$artifact_count" = 0 ]
+then
+    echo "No source artifacts given!"
+    exit 1
+fi
+
+echo "Project:   $source_project"
+echo "Ref:       $source_ref"
+echo "Updates:   $(< "$updates_file")"
+
+update_token="$(< "${ENV_UPDATE_TOKEN?no update token}")"
+update_token_name="${ENV_UPDATE_TOKEN_NAME?no update token name}"
+envs_base_dir="envs"
+mkdir -p "$envs_base_dir" || echo "Envs dir already exists"
+
+checkout_env() {
+    local env="${1?no env}"
+    local repo_url="https://${update_token_name}:${update_token}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}"
+    local env_dir="${envs_base_dir}/${env}"
+    if [ -e "$env_dir" ]
+    then
+        git -C "${env_dir}" pull --rebase >&2
+    else
+        git clone -b "$env" "$repo_url" "$env_dir" >&2
+    fi
+    readlink -f "$env_dir"
+}
+
+update_env() {
+    local env="${1?no env}"
+    local updates_file="${2?no updates file}"
+    local artifacts_file="artifacts.json"
+
+    pushd "$(checkout_env "$env")"
+    if [ -r "$artifacts_file" ]
+    then
+        local tmp_file="$(mktemp)"
+        jq -n \
+            --slurpfile artifacts_slurp "$artifacts_file" \
+            --slurpfile updates_slurp "$updates_file" \
+            '($artifacts_slurp | first) as $artifacts | $updates_slurp | first | map(select($artifacts[.name] != null)) | reduce .[] as $item ($artifacts; .[$item.name] = $item.descriptor)' \
+            > "$tmp_file"
+        mv "$tmp_file" "$artifacts_file"
+
+        if [ -n "$(git status --porcelain)" ]
+        then
+            echo "Commiting and pushing the changes ..."
+            git config user.name 'Environment Update'
+            git config user.email "environment-update@$CI_SERVER_HOST"
+            git add "$artifacts_file"
+            git commit -m "Environment updated!"
+            git push
+            echo 'done.'
+        else
+            echo "Nothing actually changed with this update."
+        fi
+    else
+        echo "No existing $artifacts_file file found, nothing to update..."
+    fi
+    popd
+    
+}
+
+source "$update_script"
+
