96613f064e628b39109ae7c4eb6231a175e93cb3
Phillip Schichtel <phillip@schich.tel>
2022-03-20 19:43:17 +0100

    action print the output file

--- offset-ports.sh
+++ offset-ports.sh
@@ -7,4 +7,5 @@ offset="${2?no port offset}"
 output="$(mktemp)"
 
 jq -r -n --argjson offset "$offset" --rawfile input "$input" '$input | split("\n") | map(select(length > 0)) | map(split("=") | .[0] + "=" + (.[1] | tonumber | . + $offset | tostring)) | .[]' > "$output"
+echo "$output"
 
