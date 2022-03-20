1f786d805ab7f2d482034078fe8d3efdb88acef3
Phillip Schichtel <phillip@schich.tel>
2022-03-20 01:52:20 +0100

    add script to combine several env files into one

---
+++ combine-env.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [ $# -le 0 ]
+then
+    echo "No files to combine!" >&2
+    exit 1
+fi
+
+tmp_file="$(mktemp)"
+compose_args+=(--env-file "$tmp_file")
+
+for f in "$@"
+do
+    if [ -r "$f" ]
+    then
+        cat "$f" >> "$tmp_file"
+        echo "" >> "$tmp_file"
+    else
+        echo "Failed to read '$f'!" >&2
+        exit 2
+    fi
+done
+
+echo "$tmp_file"
+
